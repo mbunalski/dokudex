7c8cd0db93ba0007a902d550dd4fffff4ef9c963
bun <mbunalski@gmail.com>
2024-07-31 16:47:05 -0400

    fixed some more

--- src/Display.js
+++ src/Display.js
@@ -19,8 +19,8 @@ import {PokeInfo} from './PokeInfo';
 export function Display({pokelist}){
 
     return(
-        <div class="h-5/6 ">
-        <div class="overflow-y-auto h-full justify-start">
+        <div class="h-5/6 justify-start">
+        <div class="overflow-y-auto h-5/6 justify-start ">
             <ul>
                 {pokelist[0].map(pokemon => (
                     <PokeInfo key={pokemon} pokemon={pokemon} />
